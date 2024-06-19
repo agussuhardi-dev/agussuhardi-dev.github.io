21e71642902638b5538b5a3ae5c542e87269d52b
agussuhardi <agus.suhardi@tsmid.com>
2024-06-19 21:41:03 +0700

    json converters

--- src/app/routes/converts/json/json.component.ts
+++ src/app/routes/converts/json/json.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { PageHeaderComponent } from '@shared';
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
+import * as JsonToXML from 'js2xmlparser';
+import { stringify } from 'yaml';
+
 
 @Component({
   selector: 'app-converts-json',
   templateUrl: './json.component.html',
   styleUrl: './json.component.scss',
   standalone: true,
-  imports: [PageHeaderComponent]
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule, MatRadioGroup, MatRadioButton],
+
 })
-export class ConvertsJsonComponent implements OnInit {
+export class ConvertsJsonComponent {
 
-  constructor() { }
+  favoriteSeason: string = '';
+  seasons: string[] = ['XML', 'YML'];
+  public original = '';
+  public result = '';
 
-  ngOnInit() {
+  constructor() {
+  }
+
+
+  onChange(value: string) {
+    const original = JSON.parse(this.original);
+    if (value === 'XML') {
+      this.result = this.convertXML(original);
+    }
+    if (value === 'YML') {
+      this.result = stringify(original);
+    }
+  }
+
+
+  convertXML(obj: object) {
+    const options = {
+      format: {
+        doubleQuotes: true,
+      },
+      declaration: {
+        include: false,
+      },
+    };
+    return JsonToXML.parse('root', obj, options);
   }
 
 }
